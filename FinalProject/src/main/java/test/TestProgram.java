494ef2996a698e6242f97645732bfe50fa6a95f7
lenovo <lenovo@DESKTOP-IOQ7J5S>
2024-05-01 21:39:58 +0530

    Testing

---
+++ FinalProject/src/main/java/test/TestProgram.java
@@ -0,0 +1,17 @@
+package test;
+
+public class TestProgram {
+	
+	public void print()
+	{
+	System.out.println("GoodEve");
+	}
+	public static void main(String[] args)
+	{
+		TestProgram obj=new  TestProgram();
+		obj.print();
+		
+		
+	}
+
+}
